9898cced71b322cb0cc3ddac0c540ace3543c122
OrewaKimi <kimi93.com@gmail.com>
2025-03-25 14:40:51 +0700

    add picture, rating, and title of doctor

--- lib/util/doctor_card.dart
+++ lib/util/doctor_card.dart
@@ -5,6 +5,23 @@ class DoctorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.deepPurple[100],
+      child: Column(
+        children: [
+          // Picture Of Doctor
+          Image.asset('lib/images/doctor1.jpg', height: 40),
+
+          // Rating Out Of 5
+          Row(children: [Icon(Icons.star), Text('4.9')]),
+
+          // Doctor Name
+          Text('Dr. Arle McCoy'),
+
+          // Doctor Title
+          Text('Therapist, 7 y.e.'),
+        ],
+      ),
+    );
   }
 }
