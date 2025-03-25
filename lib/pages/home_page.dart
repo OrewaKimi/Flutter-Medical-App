a3a28352e33acc2e84beab8dc6661645ea35b6e2
OrewaKimi <kimi93.com@gmail.com>
2025-03-25 10:12:23 +0700

    add components

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          // App Bar
+
+          // Card -> how do you feel?
+
+          // Search Bar
+
+          //horizintal lisview -> categories: dentist, surgeon, etc
+
+          //doctor list
+        ],
+      ),
     );
   }
 }
