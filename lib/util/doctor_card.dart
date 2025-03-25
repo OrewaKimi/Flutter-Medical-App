565972e313d66cb7f6ed1eb7fd6bc21bdf82f7df
OrewaKimi <kimi93.com@gmail.com>
2025-03-25 15:08:57 +0700

    add height, border radius

--- lib/util/doctor_card.dart
+++ lib/util/doctor_card.dart
@@ -5,22 +5,34 @@ class DoctorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurple[100],
-      child: Column(
-        children: [
-          // Picture Of Doctor
-          Image.asset('lib/images/doctor1.jpg', height: 40),
-
-          // Rating Out Of 5
-          Row(children: [Icon(Icons.star), Text('4.9')]),
-
-          // Doctor Name
-          Text('Dr. Arle McCoy'),
-
-          // Doctor Title
-          Text('Therapist, 7 y.e.'),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            // Picture Of Doctor
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child:Image.asset('lib/images/doctor1.jpg', 
+            height: 100,
+            ),
+      ),
+      
+            // Rating Out Of 5
+            Row(children: [Icon(Icons.star), Text('4.9')]),
+      
+            // Doctor Name
+            Text('Dr. Arle McCoy'),
+      
+            // Doctor Title
+            Text('Therapist, 7 y.e.'),
+          ],
+        ),
       ),
     );
   }
