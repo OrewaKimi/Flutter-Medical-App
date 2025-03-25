8ee79247cb12d26d28ad442882e38c26a45babdf
OrewaKimi <kimi93.com@gmail.com>
2025-03-25 15:30:34 +0700

    add final string and required

--- lib/util/doctor_card.dart
+++ lib/util/doctor_card.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DoctorCard extends StatelessWidget {
-  const DoctorCard({Key? key}) : super(key: key);
+  final String doctorImagePath;
+  final String rating;
+  final String doctorName;
+  final String doctorProfession;
+
+  DoctorCard({
+    required this.doctorImagePath,
+    required this.rating,
+    required this.doctorName,
+    required this.doctorProfession,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +28,28 @@ class DoctorCard extends StatelessWidget {
             // Picture Of Doctor
             ClipRRect(
               borderRadius: BorderRadius.circular(50),
-              child:Image.asset('lib/images/doctor1.jpg', 
-            height: 100,
+              child: Image.asset('lib/images/doctor1.jpg', height: 100),
             ),
-        ),
-          SizedBox(
-          height: 10,
-          ),
-      
+            SizedBox(height: 10),
+
             // Rating Out Of 5
             Row(
-              children: [Icon(Icons.star,
-              color: Colors.yellow[600],
-              ),
-              Text('4.9'),
+              children: [
+                Icon(Icons.star, color: Colors.yellow[600]),
+                Text('4.9', style: TextStyle(fontWeight: FontWeight.bold)),
               ],
             ),
 
-          SizedBox(
-          height: 10,
-          ),
+            SizedBox(height: 10),
 
             // Doctor Name
-            Text('Dr. Arlene McCoy'),
-      
+            Text(
+              'Dr. Arlene McCoy',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+
+            SizedBox(height: 5),
+
             // Doctor Title
             Text('Therapist, 7 y.e.'),
           ],
